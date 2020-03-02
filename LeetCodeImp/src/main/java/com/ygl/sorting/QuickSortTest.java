55745dc09eb901624c80af1f9111e2f2ec913776
gyang1125 <gyang1125@gmail.com>
2020-03-02 22:58:38 +0100

    quicksort changed

--- LeetCodeImp/src/main/java/com/ygl/sorting/QuickSortTest.java
+++ LeetCodeImp/src/main/java/com/ygl/sorting/QuickSortTest.java
@@ -7,35 +7,37 @@ public class QuickSortTest {
 	public static void main(String[] args) {
 		int[] array = { 5, 3, 7, 6, 4, 1, 0, 2, 9, 10, 8 };
 		printJava7(array);
-		int[] sorted_array = qsort(array, 0, array.length - 1);
-		printJava7(sorted_array);
+		qsort(array, 0, array.length - 1);
+		printJava7(array);
 //		printJava8(sorted_array);
 	}
 
-	public static int[] qsort(int arr[], int start, int end) {
-		int pivot = arr[(int) (start + Math.random() * (end - start + 1))];// ref number
+	public static void qsort(int arr[], int start, int end) {
+		int pivot = arr[start];// ref number
 		int i = start; // 左指针
 		int j = end; // 右指针
+		if (start >= end) {
+			return;
+		}
 		while (i < j) {
-			while ((i < j) && (arr[j] > pivot)) {// 右边第一个比pivot小的值
+			while ((i < j) && (arr[j] >= pivot)) {
 				j--;
 			}
-			while ((i < j) && (arr[i] < pivot)) {// 左边第一个比pivot大的值
+			while ((i < j) && (arr[i] <= pivot)) {
 				i++;
 			}
-			if ((arr[i] == arr[j]) && (i < j)) {
-				i++;
-			} else {
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
+
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+
 		}
-		if (i - 1 > start) // 左分区，左指针回撤
-			arr = qsort(arr, start, i - 1);
-		if (j + 1 < end) // 右分区，右指针回撤
-			arr = qsort(arr, j + 1, end);
-		return (arr);
+		arr[start] = arr[i];
+		arr[i] = pivot;
+
+		qsort(arr, start, j - 1);
+		qsort(arr, j + 1, end);
+
 	}
 
 	private static void printJava8(int[] sorted_array) {
