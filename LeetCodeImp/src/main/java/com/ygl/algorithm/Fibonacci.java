a871b89002ee806e4e68508bcf79b190d63d0581
DIR\guilin.yang <guilin.yang@CPX-RXB3JNRO8CG.dir.svc.accenture.com>
2020-03-11 00:12:48 +0100

    new fibonacci

--- LeetCodeImp/src/main/java/com/ygl/algorithm/Fibonacci.java
+++ LeetCodeImp/src/main/java/com/ygl/algorithm/Fibonacci.java
@@ -1,23 +1,38 @@
 package com.ygl.algorithm;
 
 /**
- * 斐波那契数列 F(n) = F(n-1) + F(n-2), n>=2
- * 1, 1, 2, 3, 5, 8
+ * 斐波那契数列 F(n) = F(n-1) + F(n-2), n>=2 1, 1, 2, 3, 5, 8
  *
  */
 public class Fibonacci {
 	public static void main(String[] args) {
-		getF(10);
+		for (int i = 0; i <= 5; i++) {
+			fib(i);
+			System.out.println("--");
+		}
 
 	}
 
 	private static void getF(int m) {
-		int temp = 0, f1=1, f2=1;
+		int temp = 0, f1 = 1, f2 = 1;
 		for (int i = 1; i <= m; i++) {
 			System.out.println(f1);
 			temp = f2;
 			f2 += f1;
 			f1 = temp;
+		}
+	}
+
+	public static int fib(int n) {
+		System.out.println("input index: " + n);
+		if (n <= 1) {
+			System.out.println("return leaf: " + n);
+			return n;
+
+		} else {
+			int sum = fib(n - 1) + fib(n - 2);
+			System.out.println("return sum: " + sum);
+			return sum;
 		}
 	}
 
