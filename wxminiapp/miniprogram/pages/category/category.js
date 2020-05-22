03c43798ad53d1dc4ef5686c689ed0db55f0a191
gyang1125 <gyang1125@gmail.com>
2020-05-22 10:45:21 +0200

    cache tech

--- wxminiapp/miniprogram/pages/category/category.js
+++ wxminiapp/miniprogram/pages/category/category.js
@@ -6,14 +6,42 @@ Page({
     leftMenuList: [],
     rightContent: [],
     currentIndex: 0,
+    scrollTop: 0,
   },
   Cates: [],
 
   onLoad: function (options) {
+    console.log("分类加载");
+
+    // 设置缓存
+    const Cates = wx.getStorageSync("cates");
+    if (!Cates) {
+      console.log("第一次取值");
+      this.getCategory();
+    } else {
+      if (Date.now() - Cates.time > 1000 * 10) {
+        console.log("超过十秒，再次取新值");
+        this.getCategory();
+      } else {
+        console.log("从缓存中取旧数据");
+        this.Cates = Cates.data;
+        let leftMenuList = this.Cates.map((v) => v.cat_name);
+        let rightContent = this.Cates[0].regions;
+        this.setData({
+          leftMenuList,
+          rightContent,
+        });
+      }
+    }
+  },
+
+  getCategory() {
     category.get().then((res) => {
       console.log(res.data);
       this.Cates = res.data;
-      let leftMenuList = this.Cates.map(v=>v.cat_name);
+      // 存入缓存
+      wx.setStorageSync("cates", { time: Date.now(), data: this.Cates });
+      let leftMenuList = this.Cates.map((v) => v.cat_name);
       let rightContent = this.Cates[0].regions;
       this.setData({
         leftMenuList,
@@ -28,7 +56,9 @@ Page({
     let rightContent = this.Cates[index].regions;
     this.setData({
       currentIndex: index,
-      rightContent
-    })
+      rightContent,
+      // 重新设置scroll标签的顶部距离
+      scrollTop: 0,
+    });
   },
 });
